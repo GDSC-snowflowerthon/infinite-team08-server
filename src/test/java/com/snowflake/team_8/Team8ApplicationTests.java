5dce5b6e2e55fd318d47756353c3baf5dcce5bb5
Seunghan Ryu <jorzor99@gmail.com>
2024-01-09 04:41:43 +0900

    setting

--- src/test/java/com/snowflake/team_8/Team8ApplicationTests.java
+++ src/test/java/com/snowflake/team_8/Team8ApplicationTests.java
@@ -3,7 +3,6 @@ package com.snowflake.team_8;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class Team8ApplicationTests {
 
 	@Test
