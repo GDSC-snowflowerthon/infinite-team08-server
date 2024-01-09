b56fb18012f1b4cdb83f389f4ae29e459894ce35
Seunghan Ryu <jorzor99@gmail.com>
2024-01-09 17:06:16 +0900

    setting

--- src/main/java/com/snowflake/team_8/Team8Application.java
+++ src/main/java/com/snowflake/team_8/Team8Application.java
@@ -2,8 +2,9 @@ package com.snowflake.team_8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Team8Application {
 
 	public static void main(String[] args) {
